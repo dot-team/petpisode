78a60ba980649799f7d4635be9ce08c7c0240227
vgotu99 <vgotu99@gmail.com>
2025-01-20 10:16:01 +0900

    design: toast 메시지를 사용하는 커스텀 훅 title 변경 (#60)

--- src/hooks/use-error-toast.ts
+++ src/hooks/use-error-toast.ts
@@ -3,7 +3,7 @@ import { toast } from './use-toast';
 
 export const useErrorToast = ({ title, description }: UseCustomToastProps) => {
     toast({
-        title: `❌ ${title}`,
+        title: `${title}`,
         description,
         variant: 'error',
         duration: 3000,
--- src/hooks/use-success-toast.ts
+++ src/hooks/use-success-toast.ts
@@ -3,7 +3,7 @@ import { toast } from './use-toast';
 
 export const useSuccessToast = ({ title, description }: UseCustomToastProps) => {
     toast({
-        title: `✅ ${title}`,
+        title: `${title}`,
         description,
         variant: 'success',
         duration: 3000,
