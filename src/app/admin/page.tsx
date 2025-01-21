a1fcb82d33e35193cc43134b982a7cf8fa6fd8b8
mgyang53 <mg960503@gmail.com>
2025-01-21 12:27:24 +0900

    fix: 관리자 공통컴포넌트 부분 주석 처리

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,48 +1,48 @@
 'use client';
 
-import { DetailViewContainer } from '@/components/AdminViewContainer';
-import { FormContainer } from '@/components/AdminFormContainer';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
-import { Textarea } from '@/components/ui/textarea';
+// import { Input } from '@/components/ui/input';
+// import { Label } from '@/components/ui/label';
+// import { DetailViewContainer } from '@/components/AdminViewContainer';
+// import { FormContainer } from '@/components/AdminFormContainer';
+// import {
+//     Select,
+//     SelectContent,
+//     SelectItem,
+//     SelectTrigger,
+//     SelectValue,
+// } from '@/components/ui/select';
+// import { Textarea } from '@/components/ui/textarea';
 
 export default function page() {
     // Example detail view
-    const detailItems = [
-        {
-            label: 'Date of birth',
-            content: '커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라',
-        },
-        {
-            label: 'Framework',
-            content: '커뮤니티라라라라',
-        },
-        {
-            label: 'Description',
-            content:
-                '커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라 커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라',
-        },
-    ];
+    // const detailItems = [
+    //     {
+    //         label: 'Date of birth',
+    //         content: '커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라',
+    //     },
+    //     {
+    //         label: 'Framework',
+    //         content: '커뮤니티라라라라',
+    //     },
+    //     {
+    //         label: 'Description',
+    //         content:
+    //             '커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라 커뮤니티라라라라 커뮤니티라라라라커뮤니티라라라라커뮤니티라라라라',
+    //     },
+    // ];
 
     return (
         <div className="space-y-10 p-6">
             {/* Detail View Example */}
-            <DetailViewContainer
+            {/* <DetailViewContainer
                 title="커뮤니티 상세"
                 items={detailItems}
                 onCancel={() => console.log('Cancel clicked')}
                 onSubmit={() => console.log('Submit clicked')}
-            />
+            /> */}
 
             {/* Form Example */}
-            <FormContainer
+            {/* <FormContainer
                 title="커뮤니티 등록/수정"
                 onCancel={() => console.log('Cancel clicked')}
                 onSubmit={() => console.log('Submit clicked')}
@@ -74,7 +74,7 @@ export default function page() {
                         className="min-h-[150px]"
                     />
                 </div>
-            </FormContainer>
+            </FormContainer> */}
         </div>
     );
 }
