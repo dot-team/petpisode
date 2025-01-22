f7929201c14501eaf3d4ad76f4aafd74206a4e64
vgotu99 <vgotu99@gmail.com>
2025-01-22 22:21:37 +0900

    feature: tanstack-query 사용을 위한 provider를 생성하고 layout.tsx에 추가 (#48)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components';
+import Providers from '@/providers/Providers';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -26,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="ko">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {children}
+                <Providers>{children}</Providers>
                 <Toaster />
             </body>
         </html>
