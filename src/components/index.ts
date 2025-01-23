765cf4feeab3cdd0dfc241d27c3c2233c0b0738b
mgyang53 <mg960503@gmail.com>
2025-01-23 10:50:41 +0900

    feature: 관리자 view 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { AdminFormContainer, AdminHeader, AdminSidebar } from './admin';
+export { AdminFormContainer, AdminViewContainer, AdminHeader, AdminSidebar } from './admin';
 export { Toaster } from './Toast/Toaster';
 export * from './ui';
