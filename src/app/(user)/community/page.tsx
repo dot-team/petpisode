26952816fa4f9e31c2a0bd0bfef8d1df24d19392
rondido <rppr001@naver.com>
2025-01-31 18:08:11 +0900

    design: 커뮤니티 리스트 card 마크업 구현 완료

--- src/app/(user)/community/page.tsx
+++ src/app/(user)/community/page.tsx
@@ -1,11 +1,44 @@
+import PostListCard from '@/components/layout/PostListCard/PostListCard';
 import PostFormCard from '@/components/layout/Postcard/PostFormCard';
 import React from 'react';
 
+const data = [
+    {
+        id: 1,
+        name: '이름',
+        profile: '',
+        thumil: '',
+        createdAt: '2025-01-29',
+        title: '제목',
+        discription: '설명입니다',
+        commentCount: '1',
+        likeCount: '3',
+        bookmarkCount: '3',
+    },
+    {
+        id: 2,
+        name: '이름',
+        profile: '',
+        thumil: '',
+        createdAt: '2025-01-29',
+        title: '제목',
+        discription: '설명입니다',
+        commentCount: '1',
+        likeCount: '3',
+        bookmarkCount: '3',
+    },
+];
+
 function CommunityPage() {
     return (
-        <div>
+        <>
             <PostFormCard />
-        </div>
+            <div className="flex flex-col items-center justify-center w-full gap-20 mt-10">
+                {data.map(value => (
+                    <PostListCard value={value} key={value.id} />
+                ))}
+            </div>
+        </>
     );
 }
 
--- src/components/layout/Postcard/PostFormCard.tsx
+++ src/components/layout/Postcard/PostFormCard.tsx
@@ -8,25 +8,29 @@ import img from '../../../../public/images/img_upload.svg';
 function PostFormCard() {
     return (
         <div>
-            <div className="flex items-center justify-center border-box">
-                <div className="w-1/2 mt-10 h-[170px] flex flex-col rounded-dot-l bg-[#FCFAF6] relative ">
-                    <div className="flex after:absolute after:top-[21%] after:right-[15px] after:w-[90%] after:border-[1px] after:border-solid after:border-[#E4E4E7] rounded-b-dot-l">
+            <div className="flex items-center justify-center ">
+                <div className="flex flex-col bg-[#FCFAF6] rounded-dot-l w-1/2 relative mt-6">
+                    <div className="flex rounded-b-dot-l after:absolute after:top-[40px] after:right-[15px] after:w-[90%] after:border-[1px] after:border-solid after:border-[#E4E4E7]">
                         <Input
                             placeholder="제목을 입력 해주세요"
-                            className={`overflow-hidden bg-transparent border-none focus-visible:ring-0 rounded-t-dot-l rounded-b-none text-2xl font-bold `}
+                            className="overflow-hidden bg-transparent border-none focus-visible:ring-0 rounded-t-dot-l rounded-b-none text-2xl font-bold"
                         />
                     </div>
-                    <div className="flex flex-col flex-1 ">
-                        <div className="flex flex-col flex-1 overflow-auto">
-                            <div className="flex flex-1 break-words rounded-b-dot-l whitespace-break-spaces focus-visible:outline-none">
-                                <p
+                    <div className="flex flex-col flex-1">
+                        <div className="flex flex-col flex-1">
+                            <div className="flex flex-1 overflow-auto rounded-b-dot-l whitespace-break-spaces focus-visible:outline-none">
+                                <div
                                     contentEditable
                                     suppressContentEditableWarning
-                                    className="border-none rounded-t-none focus:none focus-visible:ring-0 bg-[#FCFAF6] w-full text-xl p-3 focus-visible:outline-none break-all before:text-xl before:block focus:before:content-[''] before:content-space"
+                                    className="border-none rounded-t-none focus:none
+                                    focus-visible:ring-0 bg-[#FCFAF6] w-full text-xl p-3
+                                    focus-visible:outline-none break-all before:text-xl
+                                    focus:before:content-[''] before:content-space
+                                    overflow-auto max-h-64"
                                 />
                             </div>
                         </div>
-                        <div className="flex items-center justify-between px-4 mb-4 rounded-t-dot-l bg-[#FCFAF6]">
+                        <div className="flex items-center justify-between px-4 rounded-b-dot-l bg-[#FCFAF6] pb-6">
                             <Image src={img} alt="img_upload" width={24} height={24} />
                             <Button className="w-16 h-6 text-xs text-white bg-primary hover:bg-none ">
                                 등록하기
