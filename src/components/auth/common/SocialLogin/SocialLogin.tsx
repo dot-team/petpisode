48f95cb8cc9a31632e0a22708841c61d21a351ba
mgyang53 <mg960503@gmail.com>
2025-02-12 16:36:06 +0900

    design: 헤더 로그인 버튼에 로그인 다이얼로그 연결

--- src/components/auth/common/SocialLogin/SocialLogin.tsx
+++ src/components/auth/common/SocialLogin/SocialLogin.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image';
 import { Button } from '@/components';
 import Logo from '/public/images/logo.svg';
 
-function SocialLogin() {
+interface SocialLoginProps {
+    onClickEmailLogin?: (isEmail: boolean) => void;
+}
+
+function SocialLogin({ onClickEmailLogin }: SocialLoginProps) {
     return (
         <div className="flex flex-col items-center w-full max-w-sm mx-auto space-y-l">
             <div className="text-center space-y-2">
@@ -41,20 +45,30 @@ function SocialLogin() {
                     Google로 로그인
                 </Button>
 
-                <Button
-                    className="w-full bg-dot-white hover:bg-dot-white text-dot-black border"
-                    size="lg"
-                    asChild
-                >
-                    <Link href="?method=email">이메일로 로그인</Link>
-                </Button>
+                {onClickEmailLogin ? (
+                    <Button
+                        className="w-full bg-dot-white hover:bg-dot-white text-dot-black border"
+                        size="lg"
+                        onClick={() => onClickEmailLogin(true)}
+                    >
+                        이메일로 로그인
+                    </Button>
+                ) : (
+                    <Button
+                        className="w-full bg-dot-white hover:bg-dot-white text-dot-black border"
+                        size="lg"
+                        asChild
+                    >
+                        <Link href="?method=email">이메일로 로그인</Link>
+                    </Button>
+                )}
             </div>
 
             <div className="flex items-center gap-2 text-sm">
                 <Link href="/signup" className="underline">
                     회원가입
                 </Link>
-                <span className="text-muted-foreground">|</span>
+                <span>|</span>
                 <Link href="/findpw" className="text-dot-gray-dark underline">
                     비밀번호 찾기
                 </Link>
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,1 +1,2 @@
 export * from './common';
+export { default as LoginDialog } from './LoginDialog/LoginDialog';
