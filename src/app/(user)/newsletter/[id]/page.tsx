1d9afdef326474aa788e100970f2dea74a52b5fc
최영애 <choiaeyoung1001@gmail.com>
2025-02-18 23:54:37 +0900

    feature: 뉴스레터 상세조회 페이지 게시 시간을 현재 시간과 비교하여 표시

--- src/app/(user)/newsletter/[id]/page.tsx
+++ src/app/(user)/newsletter/[id]/page.tsx
@@ -9,9 +9,36 @@ function NewsLetterDetailPage() {
     const newsId = params.id;
     const news = data.find(item => item.news_id === newsId);
 
+    const formatRelativeTime = (timestamp?: string) => {
+        if (!timestamp) return '';
+        const now = new Date();
+        const time = new Date(timestamp);
+        const diffInSeconds = Math.floor((now.getTime() - time.getTime()) / 1000); // 초 단위 차이 계산
+
+        if (diffInSeconds < 60) {
+            return `${diffInSeconds}초 전`;
+        }
+
+        const diffInMinutes = Math.floor(diffInSeconds / 60);
+        if (diffInMinutes < 60) {
+            return `${diffInMinutes}분 전`;
+        }
+
+        const diffInHours = Math.floor(diffInMinutes / 60);
+        if (diffInHours < 24) {
+            return `${diffInHours}시간 전`;
+        }
+
+        const diffInDays = Math.floor(diffInHours / 24);
+        return `${diffInDays}일 전`;
+    };
+
     return (
         <div>
-            <div className="text-s">{news?.user_name}</div>
+            <div className="text-s flex gap-4">
+                <div>{news?.user_name}</div>
+                <div className="text-dot-gray-light">{formatRelativeTime(news?.created_at)}</div>
+            </div>
             <div className="text-l font-bold">{news?.title}</div>
             {news?.img_url && (
                 <div className="my-4 w-full h-[350px] overflow-hidden flex items-center justify-center">
