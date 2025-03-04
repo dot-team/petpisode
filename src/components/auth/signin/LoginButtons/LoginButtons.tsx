be12afdedec421519af6b80cd67337ce55a2970e
mgyang53 <mg960503@gmail.com>
2025-03-04 16:51:02 +0900

    design: 로그인 버튼 디자인 수정

--- src/components/auth/signin/LoginButtons/LoginButtons.tsx
+++ src/components/auth/signin/LoginButtons/LoginButtons.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 import { Button } from '@/components';
 import { URL } from '@/constants';
 import { signInWithGoogle, signInWithKakao } from '@/lib/supabase/actions';
+import Image from 'next/image';
+import { Mail } from 'lucide-react';
+import KakaoIcon from '/public/images/kakao_icon.svg';
+import GoogleIcon from '/public/images/google_icon.svg';
 
 function LoginButtons() {
     return (
@@ -9,9 +13,10 @@ function LoginButtons() {
             <form>
                 <Button
                     formAction={signInWithKakao}
-                    className="w-full bg-[#FEE500] hover:bg-[#FEE500]/80 text-dot-black"
+                    className="w-full bg-[#FEE500] hover:bg-[#FEE500] text-dot-black/80"
                     size="lg"
                 >
+                    <Image src={KakaoIcon} width={20} height={20} alt="카카오" />
                     카카오로 로그인
                 </Button>
             </form>
@@ -22,6 +27,7 @@ function LoginButtons() {
                     className="w-full bg-dot-white hover:bg-dot-white text-dot-black border"
                     size="lg"
                 >
+                    <Image src={GoogleIcon} width={20} height={20} alt="Google" />
                     Google로 로그인
                 </Button>
             </form>
@@ -29,8 +35,8 @@ function LoginButtons() {
             <Button
                 className="w-full bg-dot-white hover:bg-dot-white text-dot-black border"
                 size="lg"
-                asChild
             >
+                <Mail size={20} />
                 <Link href={`${URL.LOGIN.link}?method=email`}>이메일로 로그인</Link>
             </Button>
         </div>
