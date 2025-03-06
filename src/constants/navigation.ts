617a01f837d92f5b92eaf5b97950f8835a279083
mgyang53 <mg960503@gmail.com>
2025-03-07 02:37:07 +0900

    fix: 페이지 경로 및 네비게이션 관리 수정

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,5 +1,35 @@
-import { URL } from './url';
+import {
+    LayoutDashboard,
+    User,
+    Newspaper,
+    BookOpen,
+    Speech,
+    TriangleAlert,
+    Settings,
+} from 'lucide-react';
+import { type PageUrl, USER_PAGE, ADMIN_PAGE } from './url';
 
-const NAV_ITEM = [{ ...URL.HOME }, { ...URL.NEWSLETTER }, { ...URL.EPISODE }, { ...URL.COMMUNITY }];
+interface NavItem {
+    text: string;
+    link: PageUrl;
+    icon?: React.FC;
+    subItems?: { text: string; link: PageUrl }[];
+}
 
-export { NAV_ITEM };
+const USER_NAV_LIST: NavItem[] = [
+    { ...USER_PAGE.HOME },
+    { ...USER_PAGE.NEWSLETTER },
+    { ...USER_PAGE.EPISODE },
+    { ...USER_PAGE.COMMUNITY },
+];
+const ADMIN_NAV_LIST: NavItem[] = [
+    { ...ADMIN_PAGE.DASHBOARD, icon: LayoutDashboard },
+    { ...ADMIN_PAGE.MEMBER, icon: User },
+    { ...ADMIN_PAGE.NEWSLETTER, icon: Newspaper },
+    { ...ADMIN_PAGE.EPISODE, icon: BookOpen },
+    { ...ADMIN_PAGE.COMMUNITY, icon: Speech },
+    { ...ADMIN_PAGE.REPORT, icon: TriangleAlert },
+    { ...ADMIN_PAGE.SETTING, icon: Settings },
+];
+
+export { USER_NAV_LIST, ADMIN_NAV_LIST };
