97ef3666ea16d3bbea50c0f0c4caf98520bdde82
최영애 <choiaeyoung1001@gmail.com>
2025-04-16 23:06:36 +0900

    feature: 로딩 스피너 구현

--- src/app/admin/newsletter/collect/page.tsx
+++ src/app/admin/newsletter/collect/page.tsx
@@ -18,7 +18,7 @@ import {
     createPreNewsItems,
     deleteAllDataFromClient,
 } from '@/services';
-import { MoveRight } from 'lucide-react';
+import { Loader2, MoveRight } from 'lucide-react';
 import React, { useEffect, useMemo, useState } from 'react';
 import { createNewsItems } from './transformData';
 
@@ -36,11 +36,14 @@ function NewsletterCollect() {
     const { categoryOptions, speciesOptions } = useFetchOptions();
     const [availableData, setAvailableData] = useState<PreNewsItem[]>([]);
     const [selectedData, setSelectedData] = useState<PreNewsItem[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
     const selectedIds = new Set(selectedData.map(item => item.pre_news_id));
 
     const availablePreNews = usePreNewsItem();
+
     useEffect(() => {
         setAvailableData(availablePreNews);
+        setIsLoading(false);
     }, [availablePreNews]);
 
     const handleCheckboxChange = (news_id: string, isChecked: boolean) => {
@@ -59,7 +62,7 @@ function NewsletterCollect() {
             if (deselectedItem) {
                 setSelectedData(prev => prev.filter(item => item.pre_news_id !== news_id));
                 setAvailableData(prev =>
-                    [...prev, deselectedItem as (typeof availableData)[number]].sort(
+                    [...prev, deselectedItem].sort(
                         (a, b) => new Date(a.pubDate).getTime() - new Date(b.pubDate).getTime(),
                     ),
                 );
@@ -147,157 +150,182 @@ function NewsletterCollect() {
 
     return (
         <div className="w-4/5 mx-auto flex flex-col justify-center">
-            <div id="apiRequestSearchBar" className="flex justify-center items-center gap-3 my-6">
-                <div className="flex gap-4 bg-zinc-200 py-3 px-5">
-                    <div className="flex gap-1">
-                        <Label htmlFor="searchWord" className="flex items-center">
-                            검색어
-                        </Label>
-                        <Input
-                            id="searchWord"
-                            type="text"
-                            placeholder="검색어 입력"
-                            value={searchWord}
-                            onChange={e => setSearchWord(e.target.value)}
-                            className="w-36"
-                            variant="admin"
-                        />
-                    </div>
-                    <div className="flex gap-1">
-                        <Label htmlFor="size" className="flex items-center">
-                            개수
-                        </Label>
-                        <Select
-                            defaultValue={String(selectedSize)}
-                            onValueChange={value => setSelectedSize(Number(value))}
-                        >
-                            <SelectTrigger id="size" variant="admin" className="w-32 bg-dot-white">
-                                <SelectValue />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="1">1</SelectItem>
-                                <SelectItem value="10">10</SelectItem>
-                                <SelectItem value="50">50</SelectItem>
-                                <SelectItem value="100">100</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <div className="flex gap-1">
-                        <Label htmlFor="sort" className="flex items-center">
-                            정렬
-                        </Label>
-                        <Select defaultValue={selectedSort} onValueChange={setSelectedSort}>
-                            <SelectTrigger id="sort" variant="admin" className="w-32 bg-dot-white">
-                                <SelectValue />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="date">최신순</SelectItem>
-                                <SelectItem value="sim">정확도순</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
+            {isLoading ? (
+                <div className="flex justify-center items-center py-20">
+                    <Loader2 className="animate-spin" size={48} />
                 </div>
-                <Button onClick={onClickAPIRequestBtn} variant="admin" className="">
-                    뉴스 API 호출
-                </Button>
-            </div>
-
-            <div id="contentWrap" className="flex gap-4 justify-between">
-                <div id="beforeSelected" className="w-2/5">
-                    <div id="apiRequestSearchBar" className="flex items-center gap-4">
-                        <div className="flex gap-2">
-                            <Label htmlFor="category" className="flex items-center">
-                                카테고리
-                            </Label>
-                            <Select
-                                defaultValue={selectedCategory}
-                                onValueChange={setSelectedCategory}
-                            >
-                                <SelectTrigger
-                                    id="category"
+            ) : (
+                <>
+                    <div
+                        id="apiRequestSearchBar"
+                        className="flex justify-center items-center gap-3 my-6"
+                    >
+                        <div className="flex gap-4 bg-zinc-200 py-3 px-5">
+                            <div className="flex gap-1">
+                                <Label htmlFor="searchWord" className="flex items-center">
+                                    검색어
+                                </Label>
+                                <Input
+                                    id="searchWord"
+                                    type="text"
+                                    placeholder="검색어 입력"
+                                    value={searchWord}
+                                    onChange={e => setSearchWord(e.target.value)}
+                                    className="w-36"
                                     variant="admin"
-                                    className="w-20 bg-dot-white"
+                                />
+                            </div>
+                            <div className="flex gap-1">
+                                <Label htmlFor="size" className="flex items-center">
+                                    개수
+                                </Label>
+                                <Select
+                                    defaultValue={String(selectedSize)}
+                                    onValueChange={value => setSelectedSize(Number(value))}
                                 >
-                                    <SelectValue />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem key="all" value="all">
-                                        전체
-                                    </SelectItem>
-                                    {Object.entries(categoryOptions.categoryMap).map(
-                                        ([categoryId, categoryDescription]) => (
-                                            <SelectItem
-                                                key={categoryId}
-                                                value={categoryDescription}
-                                            >
-                                                {categoryDescription}
-                                            </SelectItem>
-                                        ),
-                                    )}
-                                </SelectContent>
-                            </Select>
+                                    <SelectTrigger
+                                        id="size"
+                                        variant="admin"
+                                        className="w-32 bg-dot-white"
+                                    >
+                                        <SelectValue />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="10">10</SelectItem>
+                                        <SelectItem value="50">50</SelectItem>
+                                        <SelectItem value="100">100</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="flex gap-1">
+                                <Label htmlFor="sort" className="flex items-center">
+                                    정렬
+                                </Label>
+                                <Select defaultValue={selectedSort} onValueChange={setSelectedSort}>
+                                    <SelectTrigger
+                                        id="sort"
+                                        variant="admin"
+                                        className="w-32 bg-dot-white"
+                                    >
+                                        <SelectValue />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="date">최신순</SelectItem>
+                                        <SelectItem value="sim">정확도순</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
                         </div>
-                        <div className="flex gap-2">
-                            <Label htmlFor="species" className="flex items-center">
-                                종
-                            </Label>
-                            <Select
-                                defaultValue={selectedSpecies}
-                                onValueChange={setSelectedSpecies}
-                            >
-                                <SelectTrigger
-                                    id="species"
-                                    variant="admin"
-                                    className="w-32 bg-dot-white"
-                                >
-                                    <SelectValue />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem key="all" value="all">
-                                        전체
-                                    </SelectItem>
-                                    {Object.entries(speciesOptions.speciesMap).map(
-                                        ([speciesId, speciesDescription]) => (
-                                            <SelectItem key={speciesId} value={speciesDescription}>
-                                                {speciesDescription}
-                                            </SelectItem>
-                                        ),
-                                    )}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                    </div>
-
-                    <AdminNewletterCollectTable
-                        header={leftHeader}
-                        data={filteredData}
-                        side="left"
-                        onCheckboxChange={handleCheckboxChange}
-                        selectedIds={selectedIds}
-                    />
-                </div>
-                <MoveRight className="mt-40" />
-                <div id="afterSelected" className="w-3/5">
-                    <div id="btnWrap" className="flex gap-4 justify-end">
-                        <Button onClick={onClickSaveDBBtn} variant="admin" className="">
-                            DB에 저장
-                        </Button>
-                        <Button onClick={onClickClearPreDataBtn} variant="admin" className="">
-                            1차 데이터 초기화
+                        <Button onClick={onClickAPIRequestBtn} variant="admin" className="">
+                            뉴스 API 호출
                         </Button>
                     </div>
 
-                    <div className="w-full">
-                        <AdminNewletterCollectTable
-                            header={rightHeader}
-                            data={selectedData}
-                            side="right"
-                            onCheckboxChange={handleCheckboxChange}
-                            selectedIds={selectedIds}
-                        />
+                    <div id="contentWrap" className="flex gap-4 justify-between">
+                        <div id="beforeSelected" className="w-2/5">
+                            <div id="apiRequestSearchBar" className="flex items-center gap-4">
+                                <div className="flex gap-2">
+                                    <Label htmlFor="category" className="flex items-center">
+                                        카테고리
+                                    </Label>
+                                    <Select
+                                        defaultValue={selectedCategory}
+                                        onValueChange={setSelectedCategory}
+                                    >
+                                        <SelectTrigger
+                                            id="category"
+                                            variant="admin"
+                                            className="w-20 bg-dot-white"
+                                        >
+                                            <SelectValue />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem key="all" value="all">
+                                                전체
+                                            </SelectItem>
+                                            {Object.entries(categoryOptions.categoryMap).map(
+                                                ([categoryId, categoryDescription]) => (
+                                                    <SelectItem
+                                                        key={categoryId}
+                                                        value={categoryDescription}
+                                                    >
+                                                        {categoryDescription}
+                                                    </SelectItem>
+                                                ),
+                                            )}
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                                <div className="flex gap-2">
+                                    <Label htmlFor="species" className="flex items-center">
+                                        종
+                                    </Label>
+                                    <Select
+                                        defaultValue={selectedSpecies}
+                                        onValueChange={setSelectedSpecies}
+                                    >
+                                        <SelectTrigger
+                                            id="species"
+                                            variant="admin"
+                                            className="w-32 bg-dot-white"
+                                        >
+                                            <SelectValue />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem key="all" value="all">
+                                                전체
+                                            </SelectItem>
+                                            {Object.entries(speciesOptions.speciesMap).map(
+                                                ([speciesId, speciesDescription]) => (
+                                                    <SelectItem
+                                                        key={speciesId}
+                                                        value={speciesDescription}
+                                                    >
+                                                        {speciesDescription}
+                                                    </SelectItem>
+                                                ),
+                                            )}
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </div>
+
+                            <AdminNewletterCollectTable
+                                header={leftHeader}
+                                data={filteredData}
+                                side="left"
+                                onCheckboxChange={handleCheckboxChange}
+                                selectedIds={selectedIds}
+                            />
+                        </div>
+                        <MoveRight className="mt-40" />
+                        <div id="afterSelected" className="w-3/5">
+                            <div id="btnWrap" className="flex gap-4 justify-end">
+                                <Button onClick={onClickSaveDBBtn} variant="admin" className="">
+                                    DB에 저장
+                                </Button>
+                                <Button
+                                    onClick={onClickClearPreDataBtn}
+                                    variant="admin"
+                                    className=""
+                                >
+                                    1차 데이터 초기화
+                                </Button>
+                            </div>
+
+                            <div className="w-full">
+                                <AdminNewletterCollectTable
+                                    header={rightHeader}
+                                    data={selectedData}
+                                    side="right"
+                                    onCheckboxChange={handleCheckboxChange}
+                                    selectedIds={selectedIds}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>
+            )}
         </div>
     );
 }
